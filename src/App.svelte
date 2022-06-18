2d71013f391737c46e3bfe75f49f41e1a7b20b8f
jesperkha <hammerjesp@gmail.com>
2022-06-18 20:31:55 +0200

    navbar component

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,14 @@
-<main>
-	<h1>Hello there</h1>
-</main>
+<script>
+	import Fonts from "./lib/Fonts.svelte"
+	import Navbar from "./lib/components/Navbar.svelte"
+</script>
+
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+	href="https://fonts.googleapis.com/css2?family=Dosis:wght@800&family=Rubik:wght@400;500&family=Source+Code+Pro&display=swap"
+	rel="stylesheet"
+/>
+
+<Fonts />
+<Navbar />
---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,52 @@
+<script>
+</script>
+
+<section>
+	<div>
+		<p id="logo">notazi</p>
+		<p id="separator" />
+
+		<ul>
+			<li><a href="/">My notes</a></li>
+			<li><a href="/">Some link</a></li>
+		</ul>
+	</div>
+</section>
+
+<style>
+	section {
+		width: 100%;
+		height: 4em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	div {
+		width: 50%;
+		height: 100%;
+		display: flex;
+		align-items: center;
+		font-weight: 500;
+		font-size: 0.8em;
+	}
+
+	#logo {
+		font-family: "Dosis";
+		font-size: 2em;
+	}
+
+	#separator {
+		margin: 1em;
+	}
+
+	ul {
+		display: flex;
+		flex-direction: row;
+	}
+
+	ul li {
+		list-style: none;
+		margin: 1em;
+	}
+</style>
